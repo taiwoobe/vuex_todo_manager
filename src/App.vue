7627715af2062753fe9f0b25eba1df0dbba84261
Taiwo <taiwo.ibidapoobe@gmail.com>
2020-02-16 17:56:48 +0100

    chore: Store Module setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <todos/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  line-height: 1.6;
+  background: #e8f7f0;
 }
+  .container  {
+    max-width: 1100px;
+    margin: auto;
+    overflow: auto;
+    padding: 0 2rem;
+  }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h3>Todos</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Todos'
+}
+</script>
+
+<style>
+
+</style>
