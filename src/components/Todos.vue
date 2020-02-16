02c108a46ed8d6abd34318decf956ac1cbd9bc79
Taiwo <taiwo.ibidapoobe@gmail.com>
2020-02-16 20:44:24 +0100

    chore: Adding the completed toggle effect.

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
       <h3>Todos</h3>
+      <div class="legend">
+          <span>Double click to mark as completed </span>
+          <span>
+              <span class="incomplete-box"></span> = Incomplete
+          </span>
+          <span>
+              <span class="complete-box"></span> = Complete
+          </span>
+      </div>
       <div class="todos">
-          <div class="todo" v-for="todo in allTodos" :key="todo.id">
+          <div class="todo" v-for="todo in allTodos" :key="todo.id" @dblclick="onDblClick(todo)" :class="{'is-complete': todo.completed}">
               {{ todo.title }}
               <i class="fas fa-trash-alt" @click="deleteTodo(todo.id)"></i>
           </div>
@@ -19,15 +28,40 @@ export default {
         this.getTodos();
     },
     methods: {
-        ...mapActions(['getTodos', 'deleteTodo'])
+        ...mapActions(['getTodos', 'deleteTodo', 'updateTodo']),
+        onDblClick(todo) {
+            const updatedTodo = {
+                id: todo.id,
+                title: todo.title,
+                completed: !todo.completed
+            }
+            this.updateTodo(updatedTodo);
+        }
     },
 }
 </script>
 
 <style lang="scss" scoped>
+    .legend {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 1rem;
+        .complete-box {
+            display: inline-block;
+            width: 10px;
+            height: 10px;
+            background: #35495e;
+        }
+        .incomplete-box {
+            display: inline-block;
+            width: 10px;
+            height: 10px;
+            background: #41b883;
+        }
+    }
     .todos {
         display: grid;
-        grid-template-columns: repeat(3, 1fr);
+        grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
         grid-gap: 1rem;
         .todo {
             border: 1px solid #ccc;
@@ -44,6 +78,10 @@ export default {
             right: 10px;
             color: #fff;
             cursor: pointer;
+        }
+        .is-complete {
+            background: #35495e;
+            color: #fff;
         }
     }
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -25,13 +25,23 @@ const actions = {
         const limit = parseInt(e.target.options[e.target.options.selectedIndex].value);
         const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
         commit('setTodos', response.data);
+    },
+    async updateTodo({commit}, updTodo) {
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`, updTodo);
+        commit('updateTodo', response.data);
     }
 };
 
 const mutations = {
     setTodos: (state, todos) => ( state.todos = todos ),
     addNewTodo: (state, todo) => state.todos.unshift(todo),
-    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
+    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id),
+    updateTodo: (state, updTodo) => {
+        const index = state.todos.findIndex(todo => todo.id === updTodo.id)
+        if(index !== -1) {
+            state.todos.splice(index, 1, updTodo)
+        }
+    }
 };
 
 export default {
